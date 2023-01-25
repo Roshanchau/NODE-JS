7e82a26398b24e9f9df0dfb8ed16827a747301dd
jfjsdlfjlsdkfj <roshanchau001@gmail.com>
2023-01-26 01:07:02 +0545

    ejs view engine part 1 net ninja

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -5,6 +5,9 @@ const express = require("express");
 //we are just invoking the express function.
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 //listening to a request to the express appp.
 app.listen(3000);
 
@@ -12,22 +15,28 @@ app.listen(3000);
 app.get("/", (req, res) => {
   // just like the res.write() method  but here we get the contentType inbuit .
   //   res.send("<p>HELLO home page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  //   res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 //respond to the get request at the url "/about"
 app.get("/about", (req, res) => {
   //   res.send("<p>HELLO about page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  //   res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
+});
+
+app.get("/blogs/create", (re1, res) => {
+  res.render("create");
 });
 
 //redirects
 
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
-});
+// app.get("/about-me", (req, res) => {
+//   res.redirect("/about");
+// });
 
 //404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
